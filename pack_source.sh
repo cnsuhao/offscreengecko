1eb2487218faadc87af6e9a7fe3733e13ffb0fb1
res2002 <res2002@450718ed-8783-43e0-9feb-de53e563b729>
2007-11-24 01:42:53 +0000

    Scripts for packing up sources, binaries(into an "SDK")

---
+++ pack_source.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+WORKDIR=out/source
+
+if [ -e ${WORKDIR} ] ; then
+  rm -rf ${WORKDIR}
+fi
+
+VERSION=`date -I`
+PACKAGE="OffscreenGecko-source-${VERSION}"
+TARNAME="${PACKAGE}.zip"
+TOP="`pwd`"
+
+${TOP}/bin/ignorantcopy.sh . ${WORKDIR}/${PACKAGE}/
+
+cd ${WORKDIR}
+rm -f ${TOP}/out/${TARNAME}
+zip -9 -r ${TOP}/out/${TARNAME} ${PACKAGE}
+
+cd ${TOP}
