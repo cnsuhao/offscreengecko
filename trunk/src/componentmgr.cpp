f6b4f0e2f0d1fe34d58a865973b3f3b84b34e6c2
res2002 <res2002@450718ed-8783-43e0-9feb-de53e563b729>
2007-11-25 08:36:19 +0000

    Added a wrapper for the Gecko component manager (the idea is to make common   component manager/registrar tasks more convenient) as well as getters for   the actual Gecko component manager, component registrar and service manager.

---
+++ trunk/src/componentmgr.cpp
@@ -0,0 +1,38 @@
+#include "defs_private.h"
+#include "componentmgr.h"
+
+#include "OffscreenGecko/componentmgr.h"
+
+#include GECKO_INCLUDE(xpcom,nsIComponentRegistrar.h)
+
+namespace OSGK
+{
+  namespace Impl
+  {
+    ComponentMgr::ComponentMgr ()
+    {
+      initResult = NS_GetComponentManager (getter_AddRefs (nsCompMgr));
+      if (NS_FAILED (initResult)) return;
+      initResult = NS_GetComponentRegistrar (getter_AddRefs (nsCompReg));
+    }
+
+    nsresult ComponentMgr::RegisterXPCOMFactory (const nsCID& aClass, 
+      const char* className, const char* contractID, nsIFactory* factory)
+    {
+      if (NS_FAILED (initResult)) return initResult;
+
+      return nsCompReg->RegisterFactory (aClass, className, contractID, factory);
+    }
+  } // namespace Impl
+} // namespace OSGK
+
+int osgk_componentmgr_register_xpcom_factory (OSGK_ComponentMgr* compMgr, 
+  const struct nsID* aClass, const char* className,
+  const char* contractID, nsIFactory* factory,
+  OSGK_GeckoResult* geckoResult)
+{
+  nsresult rc = static_cast<OSGK::Impl::ComponentMgr*> (compMgr)->
+    RegisterXPCOMFactory (*aClass, className, contractID, factory);
+  if (geckoResult) *geckoResult = rc;
+  return NS_SUCCEEDED (rc);
+}
