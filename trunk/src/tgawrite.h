6f7b2cb1ec170dd915d140fb665158eeb88d8d24
res2002 <res2002@450718ed-8783-43e0-9feb-de53e563b729>
2007-12-08 15:17:11 +0000

    Forgot to commit

---
+++ trunk/src/tgawrite.h
@@ -0,0 +1,63 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is OffscreenGecko code.
+ *
+ * The Initial Developer of the Original Code is
+ * Frank Richter <frank.richter@gmail.com>.
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#ifndef __TGAWRITE_H__
+#define __TGAWRITE_H__
+
+#include <ostream>
+
+namespace OSGK
+{
+  class TGAWriter
+  {
+    std::ostream& out;
+
+    void WriteByte (unsigned char c)
+    { out.write ((const char*)&c, 1); }
+    void WriteWord (unsigned short s)
+    { 
+      // @@@ TODO: Support big endian machines
+      out.write ((const char*)&s, 2); 
+    }
+
+    TGAWriter (std::ostream& out) : out (out) { }
+  public:
+    static bool WriteBGRAImage (int width, int height, void* data,
+      std::ostream& out);
+  };
+} // namespace OSGK
+
+#endif // __TGAWRITE_H__
